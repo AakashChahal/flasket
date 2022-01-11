6474099d69a7ab0a9ebd594b692f54a24635401d
Aakash Chahal <aakash.chahal62@gmail.com>
2022-01-11 20:58:15 +0000

    adding templates

--- market.py
+++ market.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return '<h1>Index</h1>'
+@app.route('/home')
+def home():
+    return render_template('home.html') 
