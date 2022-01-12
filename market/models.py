ac4b8427f4ea7953dd32053a2cd45a6f22b6568c
Aakash Chahal <aakash.chahal62@gmail.com>
2022-01-12 22:23:29 +0000

    creating model relationships

--- market/models.py
+++ market/models.py
@@ -1,11 +1,22 @@
 from market import db
 
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), unique=True, nullable=False)
+    email = db.Column(db.String(length=40), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=100)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=20), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=500), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'{self.name}'
