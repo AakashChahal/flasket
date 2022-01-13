3a79e0a002bddd076b1e0e837922c4aeecd6b8f4
Aakash Chahal <aakash.chahal62@gmail.com>
2022-01-13 13:32:44 +0000

    flask validations

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,6 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from market import app, db
+from flask import render_template, redirect, url_for
+from market.models import Item, User
 from market.forms import RegisterForm
 
 @app.route('/')
@@ -13,7 +13,19 @@ def products_page():
     items = Item.query.all()
     return render_template('products.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     form = RegisterForm()
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data,
+                            email=form.email.data,
+                            password=form.password1.data)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('products_page'))
+
+    if form.errors != {}:
+        for msg in form.errors.values():
+            print(msg)
+
     return render_template('register.html', form=form)
