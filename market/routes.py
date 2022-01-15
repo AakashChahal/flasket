7d39c9d0593d28ed07cdf5ce9c769edf5abed0aa
Aakash Chahal <aakash.chahal62@gmail.com>
2022-01-15 12:19:32 +0000

    project complete: selling model not working

--- market/routes.py
+++ market/routes.py
@@ -14,7 +14,9 @@ def home_page():
 def products_page():
     items = Item.query.all()
     purchase_form = PurchaseForm()
+    selling_form = SellForm()
     if request.method == 'POST':
+        # purchasing item
         purchased_item = request.form.get('purchased_item')
         purchased_object = Item.query.filter_by(name=purchased_item).first()
         if purchased_object:
@@ -23,13 +25,26 @@ def products_page():
             purchased_object.owner = current_user.id
             current_user.budget -= purchased_object.price
             db.session.commit()
-            flash(f'You bought {purchased_object.name} for {purchased_item.price}£')
+            flash(f'You bought {purchased_object.name} for {purchased_object.price}£', category="info")
+
+        # selling item
+        selling_item = request.form.get('selling_item')
+        selling_object = Item.query.filter_by(name=selling_item).first()
+        if selling_object:
+            selling_object.owner = None
+            current_user.budget += selling_object.price
+            db.session.commit()
+            flash(f'You sold {purchased_object.name} for {purchased_object.price}£', category="info")
+        else:
+            flash(f'Something went wrong', category="info")
+
 
         return redirect(url_for('products_page'))
 
     if request.method == "GET":
         items = Item.query.filter_by(owner=None)
-        return render_template('products.html', items=items, purchase_form=purchase_form)
+        owned_items = Item.query.filter_by(owner=current_user.id)
+        return render_template('products.html', items=items, owned_items=owned_items, purchase_form=purchase_form, selling_form=selling_form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
